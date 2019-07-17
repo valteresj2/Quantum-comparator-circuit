e957004fa44213b5668ce5d2eeb06307c9fa2997
Valter Eduardo <valteresj2@gmail.com>
2019-07-17 15:04:37 -0300

    Update example_Qcomparator.py
    Add comment in script of example.

--- example_Qcomparator.py
+++ example_Qcomparator.py
@@ -7,5 +7,9 @@ Created on Wed Jul 17 14:13:34 2019
 
 import classe_circuit_Qcomparator as QC
 
-QC.comparator(qubit=3).generate_result()
+QC.comparator(qubit=3,not_list=True).generate_result()
+
+# parameter not_list is to remove or not the list item to be searched for.
+# the end result is the number of times the observations were state zero and one, zero being the search item and 1 having the item in the list.
+# the parameter qubit is the size of itens in to list
 
